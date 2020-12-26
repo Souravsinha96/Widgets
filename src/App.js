cf5b0ec0bf663af6816a165c7f9b3b04bef75cac
Souravsinha96 <souravsinha1604@gmail.com>
2020-12-26 13:33:30 +0530

    Added dropdown widget using usesate, effect and ref hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordian from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -16,11 +17,45 @@ const items = [
     content: "You use React by creating components ",
   },
 ];
+
+const options = [
+  {
+    label: "The colour Red",
+    value: "red",
+  },
+  {
+    label: "The colour Green",
+    value: "green",
+  },
+  {
+    label: "A shade of blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
-    <div>
+    <div className="ui container segment">
       {/* <Accordian items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <button
+        className="ui button"
+        onClick={() => {
+          setShowDropdown(!showDropdown);
+        }}
+      >
+        Toggle
+      </button>
+      {showDropdown && (
+        <Dropdown
+          onSelectedChange={setSelected}
+          selected={selected}
+          options={options}
+        />
+      )}
     </div>
   );
 };
